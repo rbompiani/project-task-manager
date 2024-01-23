3f5569a8ea154520f9558a5ce972696226727987
Rebecca Bompiani <bompiani@vt.edu>
2024-01-22 18:16:55 -0800

    created custom avatar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import {
+  Authenticated,
+  GitHubBanner,
+  Refine,
+  WelcomePage,
+} from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -9,11 +14,13 @@ import { dataProvider, liveProvider, authProvider } from "./providers";
 import { Home, ForgotPassword, Login, Register } from "./pages";
 
 import routerBindings, {
+  CatchAllNavigate,
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout";
 
 function App() {
   return (
@@ -37,8 +44,6 @@ function App() {
               }}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
-                <Route index element={<Home />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" index element={<Login />} />
                 <Route
@@ -46,6 +51,20 @@ function App() {
                   index
                   element={<ForgotPassword />}
                 />
+                <Route
+                  element={
+                    <Authenticated
+                      key="authenticated-layout"
+                      fallback={<CatchAllNavigate to="/login" />}
+                    >
+                      <Layout>
+                        <Outlet />
+                      </Layout>
+                    </Authenticated>
+                  }
+                >
+                  <Route index element={<Home />} />
+                </Route>
               </Routes>
               <RefineKbar />
               <UnsavedChangesNotifier />
---
+++ src/components/custom-avatar.tsx
@@ -0,0 +1,24 @@
+import { Avatar as AntdAvatar, AvatarProps } from "antd";
+
+type Props = AvatarProps & { name?: string };
+
+const CustomAvatar = ({ name, style, ...rest }: Props) => {
+  return (
+    <AntdAvatar
+      alt={name}
+      size="small"
+      style={{
+        background: "#87d068",
+        display: "flex",
+        alignItems: "center",
+        border: "none",
+        ...style,
+      }}
+      {...rest}
+    >
+      JM
+    </AntdAvatar>
+  );
+};
+
+export default CustomAvatar;
