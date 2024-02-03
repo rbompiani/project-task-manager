1e5eab7f4c6c7971cf365983425b2aa21c1b8027
Rebecca Bompiani <bompiani@vt.edu>
2024-02-03 14:24:12 -0800

    created edit company route

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
 import { resources } from "./config/resources";
 import Create from "./pages/company/create";
+import EditPage from "./pages/company/edit";
 
 function App() {
   return (
@@ -70,6 +71,7 @@ function App() {
                   <Route path="/companies">
                     <Route index element={<CompanyList />} />
                     <Route path="new" element={<Create />} />
+                    <Route path="edit/:id" element={<EditPage />} />
                   </Route>
                 </Route>
               </Routes>
---
+++ src/pages/company/edit.tsx
@@ -0,0 +1,39 @@
+import CustomAvatar from "@/components/custom-avatar";
+import { UPDATE_COMPANY_MUTATION } from "@/graphql/mutations";
+import { getNameInitials } from "@/utilities";
+import { Edit, useForm } from "@refinedev/antd";
+import { Col, Form, Row } from "antd";
+
+const EditPage = () => {
+  const { saveButtonProps, formProps, formLoading, queryResult } = useForm({
+    redirect: false,
+    meta: { gqlMutation: UPDATE_COMPANY_MUTATION },
+  });
+
+  const { avatarUrl, name } = queryResult?.data?.data || {};
+
+  return (
+    <div>
+      <Row gutter={[32, 32]}>
+        <Col xs={24} xl={12}>
+          <Edit
+            isLoading={formLoading}
+            saveButtonProps={saveButtonProps}
+            breadcrumb={false}
+          >
+            <Form {...formProps} layout="vertical">
+              <CustomAvatar
+                shape="square"
+                src={avatarUrl}
+                name={getNameInitials(name || "")}
+                style={{ width: 96, height: 96, marginBottom: "24px" }}
+              />
+            </Form>
+          </Edit>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default EditPage;
