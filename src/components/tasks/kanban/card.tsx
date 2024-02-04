1df3316e97975f53f728e7b32f5f5c2863f18aa9
Rebecca Bompiani <bompiani@vt.edu>
2024-02-04 12:34:06 -0800

    delete task feature

--- src/components/tasks/kanban/card.tsx
+++ src/components/tasks/kanban/card.tsx
@@ -9,6 +9,7 @@ import {
   EyeOutlined,
   MoreOutlined,
 } from "@ant-design/icons";
+import { useDelete, useNavigation } from "@refinedev/core";
 import {
   Button,
   Card,
@@ -37,7 +38,9 @@ type ProjectCardProps = {
 
 const ProjectCard = ({ id, title, dueDate, users }: ProjectCardProps) => {
   const { token } = theme.useToken();
-  const edit = () => {};
+  const { edit } = useNavigation();
+  const { mutate } = useDelete();
+
   const dropdownItems = useMemo(() => {
     const dropdownItems: MenuProps["items"] = [
       {
@@ -45,7 +48,7 @@ const ProjectCard = ({ id, title, dueDate, users }: ProjectCardProps) => {
         key: "1",
         icon: <EyeOutlined />,
         onClick: () => {
-          edit();
+          edit("tasks", id, "replace");
         },
       },
       {
@@ -53,7 +56,9 @@ const ProjectCard = ({ id, title, dueDate, users }: ProjectCardProps) => {
         label: "Delete card",
         key: "2",
         icon: <DeleteOutlined />,
-        onClick: () => {},
+        onClick: () => {
+          mutate({ resource: "tasks", id, meta: { operation: "task" } });
+        },
       },
     ];
     return dropdownItems;
